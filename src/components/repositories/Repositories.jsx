5b317103fa7b6dd5f69a4cdb3a23d5bb9db6d1f2
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-04-24 21:10:34 +0600

    i have added here show all and hide functionality has been added

--- src/components/repositories/Repositories.jsx
+++ src/components/repositories/Repositories.jsx
@@ -1,17 +1,36 @@
-import React, { use } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import Repository from '../repository/Repository';
+import CountUp from 'react-countup';
 
 const Repositories = ({ repositoriesPromise }) => {
-    const repositories = use(repositoriesPromise)
+
+    const repositoriesData = use(repositoriesPromise)
+    const [repositories, setRepositories] = useState([])
+    const [showAll, setShowAll] = useState(false)
+    useEffect(() => {
+        if (showAll) setRepositories(repositoriesData)
+        else setRepositories(repositoriesData.slice(0, 12))
+    }, [showAll, repositoriesData])
     return (
         <div className='sm:w-7/12 container mx-auto text-center mt-6 px-2'>
-            <h2 className='text-2xl font-bold'>Total Repositories {repositories.length}</h2>
+            <h2 className='text-2xl font-bold space-x-1'>
+                <span>Total Repositories</span>
+                <span> <CountUp start={0} end={repositoriesData.length} enableScrollSpy duration={9} />+</span>
+            </h2>
             <div className='grid sm:grid-cols-2 gap-3 mt-4'>
                 {
-                    repositories.map(repository => <Repository key={repository.repo} repository={repository}></Repository>)
+                    repositories.map((repository, i) => <Repository key={i} repository={repository}></Repository>)
                 }
             </div>
-            <hr className='border-2 border-gray-500 mt-8' />
+            <div className='mt-4'>
+                <button onClick={() => {
+                    setShowAll(!showAll)
+                    showAll && window.scrollTo(0, 400)
+                }}
+                    className=' text-white rounded-sm px-2 py-1 cursor-pointer border-2 border-black hover:border-black bg-black hover:bg-white hover:text-black duration-600 transform hover:-translate-x-0'>
+                    {showAll ? 'Show Less' : 'Show All'}</button>
+            </div>
+            <hr className='border-2 border-gray-500 mt-4' />
         </div>
     );
 };
